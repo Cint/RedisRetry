62896b4c748c4c63d23ec9feb80850bd5a6f5559
Andreas Caravella <andreas.caravella@gmail.com>
2018-01-09 20:46:21 +0100

    unit tests

---
+++ test/RedisRetry.Test/RedisTransactionExtensionSpec.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using StackExchange.Redis;
+using System.Collections.Generic;
+using Xunit;
+
+namespace RedisRetry.Test
+{
+    public class RedisTransactionExtensionSpec
+    {
+        private Mock<ITransaction> transaction = new Mock<ITransaction>();
+        private int retryCount = 5;
+        private string command = "--bigkeys";
+        private ICollection<object> args = new List<object>() { new { foo = "bar" } };
+        private CommandFlags flags = CommandFlags.PreferMaster;
+
+        [Fact]
+        public void ExecuteAsyncWithRetries()
+        {
+            RedisTransactionExtension.ExecuteAsyncWithRetries(transaction.Object, flags, retryCount);
+            transaction.Verify(d => d.ExecuteAsync(flags), Times.Once);
+
+            RedisTransactionExtension.ExecuteAsyncWithRetries(transaction.Object, command, args, flags, retryCount);
+            transaction.Verify(d => d.ExecuteAsync(command, args, flags), Times.Once);
+        }
+    }
+}
